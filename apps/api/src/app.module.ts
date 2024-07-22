9a46e2f11be98b7b42cebb1c0a03636d160d539e
Floriian <lovasifloriian@outlook.hu>
2024-07-22 18:51:23 +0200

    api: env validation.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { EnvModule } from './env/env.module';
+import { ConfigModule } from '@nestjs/config';
+import { validateEnv } from './env/env.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: validateEnv,
+      isGlobal: true,
+    }),
+    EnvModule,
+  ],
 })
 export class AppModule {}
---
+++ apps/api/src/env/env.schema.ts
@@ -0,0 +1,20 @@
+import { IsNotEmpty, validateSync } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+export class EnvSchema {
+  @IsNotEmpty()
+  DATABASE_URL: string;
+}
+
+export const validateEnv = (env: Record<string, unknown>) => {
+  const converted = plainToInstance(EnvSchema, env, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(converted);
+
+  const errorMessages = errors.map(
+    (e) => `\x1b[41m ${Object.values(e.constraints)} \x1b[0m \n\n`,
+  );
+
+  if (errors.length > 0) throw new Error(errorMessages.toString());
+  return converted;
+};
