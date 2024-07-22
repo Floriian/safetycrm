74853b5e964e31750486d2a320d7b4e1aa490cc8
Floriian <lovasifloriian@outlook.hu>
2024-07-22 19:04:51 +0200

    env and setup typeorm in api.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,14 +2,20 @@ import { Module } from '@nestjs/common';
 import { EnvModule } from './env/env.module';
 import { ConfigModule } from '@nestjs/config';
 import { validateEnv } from './env/env.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './configs/typeorm.config';
+
+const env = process.env.NODE_ENV;
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: validateEnv,
       isGlobal: true,
+      envFilePath: !env ? '.env' : `.env.${env}`,
     }),
     EnvModule,
+    TypeOrmModule.forRootAsync(typeormConfig),
   ],
 })
 export class AppModule {}
---
+++ apps/api/src/configs/typeorm.config.ts
@@ -0,0 +1,37 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { join } from 'path';
+import { EnvModule } from 'src/env/env.module';
+import { EnvService } from 'src/env/env.service';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const typeormConfig: TypeOrmModuleAsyncOptions = {
+  imports: [EnvModule],
+  inject: [EnvService],
+  useFactory: (envService: EnvService) => {
+    const config = buildDataSourceOptinos(envService);
+    return {
+      ...config,
+    };
+  },
+};
+
+export function buildDataSourceOptinos(
+  envService: EnvService,
+): DataSourceOptions {
+  return {
+    url: envService.get('DATABASE_URL'),
+    type: 'postgres',
+    synchronize: false,
+  };
+}
+
+const dataSourceOptions = buildDataSourceOptinos(
+  new EnvService(new ConfigService()),
+);
+
+export default new DataSource({
+  ...dataSourceOptions,
+  migrations: [join(__dirname, '../migrations/*.{ts,js}')],
+  entities: [join(__dirname, '../**/**/**.entity.{js,ts}')],
+});
