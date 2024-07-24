ea875e16622cf03918d321d29e0ed1b2d48eeb86
Floriian <lovasifloriian@outlook.hu>
2024-07-24 18:38:59 +0200

    update env schema

--- apps/api/src/env/env.schema.ts
+++ apps/api/src/env/env.schema.ts
@@ -1,8 +1,21 @@
-import { IsNotEmpty, validateSync } from 'class-validator';
+import { IsNotEmpty, IsNumberString, validateSync } from 'class-validator';
 import { plainToInstance } from 'class-transformer';
 export class EnvSchema {
   @IsNotEmpty()
-  DATABASE_URL: string;
+  DATABASE_USERNAME: string;
+
+  @IsNotEmpty()
+  DATABASE_PASSWORD: string;
+
+  @IsNotEmpty()
+  DATABASE_NAME: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  DATABASE_PORT: string;
+
+  @IsNotEmpty()
+  DATABASE_HOST: string;
 
   @IsNotEmpty()
   AUTH_SECRET: string;
