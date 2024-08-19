2d60c615d464aa946cb50d6ec9b45bd6dcb7216a
Floriian <lovasifloriian@outlook.hu>
2024-08-19 13:13:20 +0200

    Extends auth error with forbidden

--- apps/frontend/src/lib/auth/auth-error.ts
+++ apps/frontend/src/lib/auth/auth-error.ts
@@ -6,3 +6,11 @@ export const SesionExpiredError = class SessionExpiredError extends Error {
     this.name = "SessionExpiredError";
   }
 };
+
+export const FORBIDDEN_RESOURCE_ERROR = "You are not authorized.";
+export const ForbiddenResourceError = class ForbiddenResourceError extends Error {
+  constructor() {
+    super(FORBIDDEN_RESOURCE_ERROR);
+    this.name = "ForbiddenResourceError";
+  }
+};
